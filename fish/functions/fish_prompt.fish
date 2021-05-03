14ae39fc13ce698606f9459ecfae49447241d85f
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-05-03 18:19:40 +0800

    Delete Spacefish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,33 +1,96 @@
+# Defined interactively
 function fish_prompt
-	# Store the exit code of the last command
-	set -g sf_exit_code $status
-	set -g SPACEFISH_VERSION 2.7.0
+    set -l __last_command_exit_status $status
 
-	# ------------------------------------------------------------------------------
-	# Configuration
-	# ------------------------------------------------------------------------------
+    if not set -q -g __fish_robbyrussell_functions_defined
+        set -g __fish_robbyrussell_functions_defined
+        function _git_branch_name
+            set -l branch (git symbolic-ref --quiet HEAD 2>/dev/null)
+            if set -q branch[1]
+                echo (string replace -r '^refs/heads/' '' $branch)
+            else
+                echo (git rev-parse --short HEAD 2>/dev/null)
+            end
+        end
 
-	__sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
-	__sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
-	__sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
-	__sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
-	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
-	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
-	__sf_util_set_default SPACEFISH_PROMPT_ORDER time user dir host git package node ruby golang php rust haskell julia elixir docker aws venv conda pyenv dotnet kubecontext exec_time line_sep battery vi_mode jobs exit_code char
+        function _is_git_dirty
+            echo (git status -s --ignore-submodules=dirty 2>/dev/null)
+        end
 
-	# ------------------------------------------------------------------------------
-	# Sections
-	# ------------------------------------------------------------------------------
+        function _is_git_repo
+            type -q git
+            or return 1
+            git rev-parse --git-dir >/dev/null 2>&1
+        end
 
-	# Keep track of whether the prompt has already been opened
-	set -g sf_prompt_opened $SPACEFISH_PROMPT_FIRST_PREFIX_SHOW
+        function _hg_branch_name
+            echo (hg branch 2>/dev/null)
+        end
 
-	if test "$SPACEFISH_PROMPT_ADD_NEWLINE" = "true"
-		echo
-	end
+        function _is_hg_dirty
+            echo (hg status -mard 2>/dev/null)
+        end
 
-	for i in $SPACEFISH_PROMPT_ORDER
-		eval __sf_section_$i
-	end
-	set_color normal
+        function _is_hg_repo
+            fish_print_hg_root >/dev/null
+        end
+
+        function _repo_branch_name
+            _$argv[1]_branch_name
+        end
+
+        function _is_repo_dirty
+            _is_$argv[1]_dirty
+        end
+
+        function _repo_type
+            if _is_hg_repo
+                echo hg
+                return 0
+            else if _is_git_repo
+                echo git
+                return 0
+            end
+            return 1
+        end
+    end
+
+    set -l cyan (set_color -o cyan)
+    set -l brcyan (set_color -o brcyan)
+    set -l yellow (set_color -o yellow)
+    set -l red (set_color -o red)
+    set -l green (set_color -o green)
+    set -l brgreen (set_color -u brgreen)
+    set -l magenta (set_color -o magenta)
+    set -l brmagenta (set_color -u brmagenta)
+    set -l blue (set_color -o blue)
+    set -l normal (set_color normal)
+
+    set -l arrow_color "$green"
+    if test $__last_command_exit_status != 0
+        set arrow_color "$red"
+    end
+
+    set -l cwd $magenta(basename (prompt_pwd))
+
+    set -l arrow "$arrow_color➜ "
+    if test "$USER" = root
+        set arrow "$arrow_color# "
+    end
+
+    set -l repo_info
+    if set -l repo_type (_repo_type)
+        set -l repo_branch $green(_repo_branch_name $repo_type)
+        set repo_info "$magenta ($repo_branch$magenta)"
+
+        set -l dirty (_is_repo_dirty $repo_type)
+        if test -n "$dirty"
+            set -l repo_branch $red(_repo_branch_name $repo_type)
+            set repo_info "$magenta ($repo_branch $red✗$magenta)"
+            # set -l dirty "$red ✗"
+            # set repo_info "$repo_info$dirty"
+        end
+    end
+
+    echo -n -s  $cwd $repo_info $normal ' ' $arrow $normal
 end
